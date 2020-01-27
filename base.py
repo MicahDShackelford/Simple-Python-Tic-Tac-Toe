efb03cb18a0f50527818cd20b0466a99cb73e7ae
Micah <micahdshackelford@gmail.com>
2020-01-26 19:00:17 -0500

    Layed out base structure

---
+++ base.py
@@ -0,0 +1,4 @@
+# This is a basic python tic tac toe game.
+# The structure of the board is a 1d array ['_','_','_','_','_','_','_','_','_'] <- Base board
+# A board will be populated by Xs and Os as the game progresses 
+
